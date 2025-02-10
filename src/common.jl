278ec86c8322e7f1746c6ae09cc0990208543076
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-02-10 16:42:50 +0100

    New functionalities

--- src/common.jl
+++ src/common.jl
@@ -1,5 +1,49 @@
 module Common
 
+using DataFrames
+
+
+const MAKIETHEME = Theme(
+	fontsize = 10,
+
+	# https://juliagraphics.github.io/Colors.jl/stable/namedcolors/
+	palette = (
+		color = [
+			# Start with classy black...
+			:black,
+
+			# Classic colors...
+			:red, 
+			:blue,
+			:green,
+
+			# Degraded classics...
+			:tomato1,
+			:slateblue1,
+			:chartreuse1,
+		],
+	),
+
+    Axis = (
+		# Background:
+		backgroundcolor = :gray98,
+	
+		# Grids:
+        xgridcolor = :gray20,
+        ygridcolor = :gray20,
+		xgridstyle = :dot,
+		ygridstyle = :dot,
+		xgridwidth = 0.8,
+		ygridwidth = 0.8,
+
+		# Spines:
+        leftspinevisible   = true,
+        rightspinevisible  = true,
+        bottomspinevisible = true,
+        topspinevisible    = true,
+    )
+)
+
 function remove_spaces(list)
     return filter(x -> x != "", list)
 end
@@ -7,6 +51,12 @@ end
 function uncomment(line; comment_char = "!")
     !startswith(line, comment_char) && return line
     return line[length(comment_char)+1:end]
+end
+
+function set_type!(df, column, totype)
+	!(column in names(df)) && return
+	transform!(df, column => ByRow(x -> convert(totype, x)) => column)
+	return nothing
 end
 
 end # (Common)
--- src/elmer.jl
+++ src/elmer.jl
@@ -6,7 +6,73 @@ using CairoMakie
 
 import ..Common
 
+#######################################################################
+# API
+#######################################################################
+
 export get_convergence_history
+export plot_nonlinear_convergence
+
+function get_convergence_history(fname)
+    data = readdlm(fname; skipstart=1)
+    header = get_convergence_header(fname)
+    df = DataFrame(data, header)
+
+	Common.set_type!(df, "solver", Int64)
+	Common.set_type!(df, "ss/ns", Int64)
+	Common.set_type!(df, "timestep", Int64)
+	Common.set_type!(df, "coupled", Int64)
+	Common.set_type!(df, "nonlin", Int64)
+
+    return df
+end
+
+function plot_nonlinear_convergence(table, solver_no;
+        convergence = 1.0e-6,
+        xlimits = nothing,
+        ylimits = (1e-8, 1e-2),
+        full_plot = true
+    )
+    df = select_solver(table, solver_no)
+    last_iter = find_last_iter!(df)
+
+    with_theme(Common.MAKIETHEME) do
+        f = Figure()
+        ax = Axis(f[1, 1]; yscale = log10)
+
+        if !isnothing(convergence)
+            hlines!(ax, convergence; color = :blue, linestyle = :dash)
+        end
+
+        if full_plot
+            x, y = df[!, "pseudotime"], df[!, "change"]
+            lines!(ax, x, y; color = :black)
+            
+            x, y = last_iter[!, "pseudotime"], last_iter[!, "change"]
+            scatter!(ax, x, y; color = :red)
+        else
+            x, y = last_iter[!, "pseudotime"], last_iter[!, "change"]
+            lines!(ax, x, y; color = :black)
+        end
+
+        if !isnothing(xlimits)
+            xlims!(ax, xlimits)
+        end
+
+        if !isnothing(ylimits)
+            ylims!(ax, ylimits)
+        end
+
+        ax.xlabel = "Time step"
+        ax.ylabel = "Change"
+        
+        f, ax
+    end
+end
+
+#######################################################################
+# INTERNALS
+#######################################################################
 
 function get_convergence_header(fname)
     open(fname) do fp
@@ -26,10 +92,31 @@ function get_convergence_header(fname)
     end
 end
 
-function get_convergence_history(fname)
-    data = readdlm(fname; skipstart=1)
-    header = get_convergence_header(fname)
-    return DataFrame(data, header)
+function select_solver(df, solver_no; dropcol = true)
+	rows = subset(df, "solver" => ByRow(x -> x == solver_no))
+	dropcol && return select(rows, Not("solver"))
+	return rows
+end
+
+function find_last_iter!(df)
+	grouped_df = groupby(df, "timestep")
+	max_values = combine(grouped_df, "nonlin" => maximum => "idx")
+	
+	last_iter = innerjoin(df, max_values, on = ["timestep", "nonlin" => "idx"])
+	create_pseudotime!(df, last_iter)
+    
+    # Repeat to get with `pseudotime` column.
+	last_iter = innerjoin(df, max_values, on = ["timestep", "nonlin" => "idx"])
+	return last_iter
+end
+
+function create_pseudotime!(df, last_iter)
+	time_dict = Dict(zip(last_iter[!, "timestep"], last_iter[!, "nonlin"]))
+	
+	func(timestep, nonlin) = timestep + (nonlin - 2) / time_dict[timestep]
+	
+	transform!(df, ["timestep", "nonlin"] => ByRow(func) => "pseudotime")
+	return nothing
 end
 
 end # (Elmer)
